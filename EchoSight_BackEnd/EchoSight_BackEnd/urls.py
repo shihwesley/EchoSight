8b90c443616878832ef7fe79e777c5f2dcc71012
Wesley Shih <shih.wesley@gmail.com>
2023-11-15 11:55:32 -0800

    Update urls.py

--- EchoSight_BackEnd/EchoSight_BackEnd/urls.py
+++ EchoSight_BackEnd/EchoSight_BackEnd/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health', views.health_check, name='health_check'),
     path('api/upload/', include('uploads.urls')),
 ]
