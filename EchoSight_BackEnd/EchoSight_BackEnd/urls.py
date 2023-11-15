d8b033be7d55acf95938c1454a06e96440d0e437
Wesley Shih <shih.wesley@gmail.com>
2023-11-15 12:43:33 -0800

    Update urls.py

--- EchoSight_BackEnd/EchoSight_BackEnd/urls.py
+++ EchoSight_BackEnd/EchoSight_BackEnd/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import views  # Import views from the project level
+from uploads import views
 
 
 
